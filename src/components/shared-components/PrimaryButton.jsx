997377541e06a9feef6d24209f48f4fd764810ec
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2025-01-12 03:01:31 +0600

    primary button design added

---
+++ src/components/shared-components/PrimaryButton.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+const PrimaryButton = ({
+  title = "Click Me",
+  onClick = () => {},
+  height = "50px",
+  width = "150px",
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        height,
+        width,
+        backgroundColor: "#3498db",
+        color: "white",
+        border: "none",
+        borderRadius: "5px",
+        fontSize: "16px",
+        cursor: "pointer",
+        transition: "background-color 0.3s",
+      }}
+      className="hover:bg-blue-600 focus:outline-none"
+    >
+      {title}
+    </button>
+  );
+};
+
+PrimaryButton.propTypes = {
+  title: PropTypes.string,
+  onClick: PropTypes.func,
+  height: PropTypes.string,
+  width: PropTypes.string,
+};
+
+export default PrimaryButton;
