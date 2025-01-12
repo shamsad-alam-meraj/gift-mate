e8dc390e56c51cf78bc149e047d91dc33edbc19c
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2025-01-13 00:43:19 +0600

    section header comp added

--- src/components/shared-components/SectionHeader.jsx
+++ src/components/shared-components/SectionHeader.jsx
@@ -1,8 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+
 /* eslint-disable react/prop-types */
 const SectionHeader = ({ title }) => {
+  const titleRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (titleRef.current) {
+      observer.observe(titleRef.current);
+    }
+
+    return () => {
+      if (titleRef.current) {
+        observer.unobserve(titleRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    // Trigger GSAP animation when title becomes visible
+    if (isVisible) {
+      gsap.fromTo(
+        titleRef.current,
+        { y: -50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          ease: "bounce.out",
+        }
+      );
+    }
+  }, [isVisible]);
+
   return (
     <div>
-      <h2 className="text-2xl font-semibold text-primary">{title}</h2>
+      <h2 ref={titleRef} className="text-2xl font-semibold text-primary">
+        {title}
+      </h2>
     </div>
   );
 };
