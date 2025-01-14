ebbb678e53c960003f0d18c477eee1f92e1ddbbe
Shamsad Alam Meraj <shamsadalammeraj9@outlook.com>
2025-01-14 21:10:06 +0600

    firebase config

--- firebase.init.js
+++ firebase.init.js
@@ -1,10 +1,7 @@
 /* eslint-disable no-undef */
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -14,5 +11,8 @@ const firebaseConfig = {
   appId: process.env.FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
